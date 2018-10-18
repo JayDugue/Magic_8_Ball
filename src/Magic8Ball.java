98b4c2bb4aa27724ab69a69dd8ad0d5df88537d4
Jayden Dugue <jayden.dugue.2021@student.lhschools.org>
2018-10-18 08:33:42 -0400

    more changes were made

--- src/Magic8Ball.java
+++ src/Magic8Ball.java
@@ -2,49 +2,89 @@ import java.util.Scanner;
 
 public class Magic8Ball {
 
-    public static void main(String[]args) {
+    public static void main(String[] args) {
         //code goes in here
         Scanner keyboard;
-        int testing;
+
         String question;
+        int pickedNumber;
+        int userInput = 1;
+
+        do {
 
 
-        keyboard = new Scanner(System.in);
-        System.out.println("What's your question");
-        question = keyboard.nextLine();
-        System.out.println("Give me a number between 1 and 100");
-        testing = keyboard.nextInt();
-        if ((0 <= testing) && (testing <= 10)) {
-            System.out.println("outlook doesn't look good");
-        }
-        if ((11 <= testing) && (testing <= 20)) {
-            System.out.println("Without a doubt");
-        }
-        if ((21 <= testing) && (testing <= 30)) {
-            System.out.println("Definitely");
-        }
-        if ((31 <= testing) && (testing <= 40)) {
-            System.out.println("Never");
-        }
-        if ((41 <= testing) && (testing <= 50)) {
-            System.out.println("Most likely");
-        }
-        if ((51 <= testing) && (testing <= 60)) {
-            System.out.println("All signs point to No");
-        }
-        if ((61 <= testing) && (testing <= 70)) {
-            System.out.println("Not going to happen");
-        }
-        if ((71 <= testing) && (testing <= 80)) {
-            System.out.println("It is certain");
-        }
-        if ((81 <= testing) && (testing <= 90)) {
-            System.out.println("You may not rely on it");
-        }
-        if ((91 <= testing) && (testing <= 100)) {
-            System.out.println("Your future ahead looks great");
-        }
+            pickedNumber = (int) (Math.random() * 100);
+
+            System.out.println(pickedNumber);
+            keyboard = new Scanner(System.in);
+            System.out.println("What's your question");
+            question = keyboard.nextLine();
+            int maxTries = 10;
+            for (int tries = 0; tries <= maxTries; tries++) {
+                if ((0 <= pickedNumber) && (pickedNumber <= 10)) {
+                    System.out.println("Your future doesn't look bright");
+                    System.out.println("Do u want to play again, 1 for yes, 2 for no?");
+                    userInput = keyboard.nextInt();
+                    break;
+                }
+                if ((11 <= pickedNumber) && (pickedNumber <= 20)) {
+                    System.out.println("Without a doubt");
+                    System.out.println("Do u want to play again, 1 for yes, 2 for no?");
+                    userInput = keyboard.nextInt();
+                    break;
+                }
+                if ((21 <= pickedNumber) && (pickedNumber <= 30)) {
+                    System.out.println("Definitely");
+                    System.out.println("Do u want to play again, 1 for yes, 2 for no?");
+                    userInput = keyboard.nextInt();
+                    break;
+                }
+                if ((31 <= pickedNumber) && (pickedNumber <= 40)) {
+                    System.out.println("Not likely");
+                    System.out.println("Do u want to play again, 1 for yes, 2 for no?");
+                    userInput = keyboard.nextInt();
+                    break;
+                }
+                if ((41 <= pickedNumber) && (pickedNumber <= 50)) {
+                    System.out.println("No");
+                    System.out.println("Do u want to play again, 1 for yes, 2 for no?");
+                    userInput = keyboard.nextInt();
+                    break;
+                }
+                if ((51 <= pickedNumber) && (pickedNumber <= 60)) {
+                    System.out.println("Yes");
+                    System.out.println("Do u want to play again, 1 for yes, 2 for no?");
+                    userInput = keyboard.nextInt();
+                    break;
+                }
+                if ((61 <= pickedNumber) && (pickedNumber <= 70)) {
+                    System.out.println("Very likely");
+                    System.out.println("Do u want to play again, 1 for yes, 2 for no?");
+                    userInput = keyboard.nextInt();
+                    break;
+                }
+                if ((71 <= pickedNumber) && (pickedNumber <= 80)) {
+                    System.out.println("Your future seems good");
+                    System.out.println("Do u want to play again, 1 for yes, 2 for no?");
+                    userInput = keyboard.nextInt();
+                    break;
+                }
+                if ((81 <= pickedNumber) && (pickedNumber <= 90)) {
+                    System.out.println("Nope");
+                    System.out.println("Do u want to play again, 1 for yes, 2 for no?");
+                    userInput = keyboard.nextInt();
+                    break;
+                }
+                if ((91 <= pickedNumber) && (pickedNumber <= 100)) {
+                    System.out.println("Sorry, but No");
+                    System.out.println("Do u want to play again, 1 for yes, 2 for no?");
+                    userInput = keyboard.nextInt();
+                    break;
+
+                }
+            }
 
 
+        }while (1 == userInput);
     }
 }
